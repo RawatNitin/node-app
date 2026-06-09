a063aaf6a4f3cda9d4146369163448d998bd37fe
RawatNitin <nitin.raw@gmail.com>
2026-06-09 19:05:07 +0530

    404 page

--- app.js
+++ app.js
@@ -10,4 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRouter);
 app.use(shopperRouter);
 
+// If any path was not matched, request reached here
+app.use((req, res) => {
+  res.status(400).send(`<html><h1>Pagenotfound!! 404!!</h1></html>`);
+});
+
 app.listen(8080);
