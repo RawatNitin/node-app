e8a847a74d2042fbe68e03d2a04ebe72eae5e2f0
RawatNitin <nitin.raw@gmail.com>
2026-06-09 20:03:37 +0530

    serve css files

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRouter);
 app.use(shopRouter);
 
