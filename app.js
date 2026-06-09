762e4068b351e86831b1dd2e1d8602bff5bf7a74
RawatNitin <nitin.raw@gmail.com>
2026-06-09 17:39:00 +0530

    add middleware

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("req in middleware");
+  next(); //Allows the request to continue the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("req in middleware");
+  res.send(`<html><h1>Response from HTML!!</h1></html>`);
+});
+
 app.listen(8080);
