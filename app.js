f9d2f223a7c45aebe264f3f6164ba075e4b9db5c
RawatNitin <nitin.raw@gmail.com>
2026-06-09 18:22:34 +0530

    added bodyparser and routes

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("req in middleware");
-  next(); //Allows the request to continue the next middleware in line
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// req.url shold start with whatever path is there in the middleware
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-app.use((req, res, next) => {
-  console.log("req in middleware");
-  res.send(`<html><h1>Response from HTML!!</h1></html>`);
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<html>
+        <head><title>Add Product</title></head>
+        <h1>Add Product</h1>
+        <form action="/product" method="POST">
+        <input type="text" name="product"/>
+        <button type="submit">Add Product</button>
+        </form>
+        </html>`,
+  );
+});
+
+app.use("/", (req, res, next) => {
+  res.send(
+    `<html>
+              <head><title>Express!!</title></head>
+              <h1>Express Home Page!! </h1>
+              </html>`,
+  );
 });
 
 app.listen(8080);
