e3eccaed973cdb124271d4dd77b99fbd471b595f
RawatNitin <nitin.raw@gmail.com>
2026-06-10 18:07:16 +0530

    html css

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const adminRouter = require("./routes/admin");
-const shopRouter = require("./routes/shop");
-const path = require("path");
-const rootPath = require("./utils/path");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRouter);
-app.use(shopRouter);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-// If any path was not matched, request reached here
-app.use((req, res) => {
-  res.status(400).sendFile(path.join(rootPath, "views", "404.html"));
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(8080);
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const router = express.Router();
-const path = require("path");
-const rootPath = require("../utils/path");
+const path = require('path');
 
-// In case of app.use or router.use req.url should start with whatever path is there in the middleware
-// In case of post, put, get, patch and other methods, methods it does exact match
-router.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootPath, "views", "add-product.html"));
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
