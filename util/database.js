391f552e01166d52264e75a677a2de5b3837ac0e
RawatNitin <nitin.raw@gmail.com>
2026-06-17 15:51:59 +0530

    Implement Sequelize ORM for database management and refactor product handling
    - Updated app.js to integrate Sequelize for database synchronization and connection.
    - Refactored product model to utilize Sequelize for data operations, replacing previous file-based methods.
    - Modified product and cart controllers to handle asynchronous operations with Sequelize.
    - Enhanced error handling in product creation, updating, and deletion processes.
    - Added jsconfig.json for improved JavaScript tooling support.
    - Updated package.json and package-lock.json to include Sequelize as a dependency.
    - Adjusted views to reflect changes in product data handling and improved layout styling.

--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
+const sequelize = new Sequelize({
   database: "node-complete",
+  username: "root",
   password: "Pegasus@1111",
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
