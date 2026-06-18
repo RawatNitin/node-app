642fbcf4a9da3bfcab72df7fd96fe7f50b846cdb
RawatNitin <nitin.raw@gmail.com>
2026-06-18 21:23:57 +0530

    adding session

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require("dotenv").config({ path: ".local.env" });
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -11,21 +13,34 @@ const User = require("./models/user");
 const mongoUser = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
 
-const uri = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.dphbp9y.mongodb.net/shop?appName=Cluster0`;
+const MONGODB_URI = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.dphbp9y.mongodb.net/shop?appName=Cluster0`;
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  }),
+);
 
 app.use((req, res, next) => {
-  User.findById("6a33db718f844a1b6dd748e3")
+  User.findOne()
     .then((user) => {
       req["user"] = user;
       next();
@@ -35,11 +50,12 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(uri)
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
