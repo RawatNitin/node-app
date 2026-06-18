decd3927dba0dfc883a10f9a8cb895d333338556
RawatNitin <nitin.raw@gmail.com>
2026-06-18 13:27:48 +0530

    Refactor application to integrate Mongoose for MongoDB operations
    - Updated app.js to establish a connection with MongoDB using Mongoose.
    - Refactored user and product models to utilize Mongoose for data management, replacing Sequelize.
    - Enhanced controllers to handle user and product operations with Mongoose, including adding users and managing cart functionality.
    - Updated routes and views to reflect changes in data structure and improve user interaction.
    - Removed legacy Sequelize models and related code for a cleaner implementation.

--- models/product.js
+++ models/product.js
@@ -1,76 +1,78 @@
-const { ObjectId } = require("mongodb");
-const getDb = require("../util/database").getDb;
+// const mongodb = require('mongodb');
+// const getDb = require('../util/database').getDb;
 
-class Product {
-  constructor(title, price, description, imageUrl, id) {
-    this.title = title;
-    this.price = price;
-    this.description = description;
-    this.imageUrl = imageUrl;
-    this._id = id ? new ObjectId(id) : null;
-  }
+// class Product {
+//   constructor(title, price, description, imageUrl, id, userId) {
+//     this.title = title;
+//     this.price = price;
+//     this.description = description;
+//     this.imageUrl = imageUrl;
+//     this._id = id ? new mongodb.ObjectId(id) : null;
+//     this.userId = userId;
+//   }
 
-  save() {
-    const db = getDb();
-    let dbOp;
-    if (this._id) {
-      dbOp = db
-        .collection("products")
-        .updateOne({ _id: this._id }, { $set: this });
-    } else {
-      dbOp = db.collection("products").insertOne(this);
-    }
-    return dbOp
-      .then((result) => {
-        console.log("Updated Product", result);
-        return result;
-      })
-      .catch((err) => {
-        console.log(err);
-        throw err;
-      });
-  }
+//   save() {
+//     const db = getDb();
+//     let dbOp;
+//     if (this._id) {
+//       // Update the product
+//       dbOp = db
+//         .collection('products')
+//         .updateOne({ _id: this._id }, { $set: this });
+//     } else {
+//       dbOp = db.collection('products').insertOne(this);
+//     }
+//     return dbOp
+//       .then(result => {
+//         console.log(result);
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
 
-  static fetchAll() {
-    const db = getDb();
-    return db
-      .collection("products")
-      .find()
-      .toArray()
-      .then((products) => {
-        console.log(products);
-        return products;
-      })
-      .catch((err) => {
-        console.log(err);
-        throw err;
-      });
-  }
+//   static fetchAll() {
+//     const db = getDb();
+//     return db
+//       .collection('products')
+//       .find()
+//       .toArray()
+//       .then(products => {
+//         console.log(products);
+//         return products;
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
 
-  static findById(productId) {
-    const db = getDb();
-    return db
-      .collection("products")
-      .find({ _id: new ObjectId(productId) })
-      .next()
-      .then((product) => {
-        return product;
-      })
-      .catch((err) => {
-        console.log("Error in finding product", err);
-        throw err;
-      });
-  }
+//   static findById(prodId) {
+//     const db = getDb();
+//     return db
+//       .collection('products')
+//       .find({ _id: new mongodb.ObjectId(prodId) })
+//       .next()
+//       .then(product => {
+//         console.log(product);
+//         return product;
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
 
-  static deleteById(prodId) {
-    const db = getDb();
+//   static deleteById(prodId) {
+//     const db = getDb();
+//     return db
+//       .collection('products')
+//       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+//       .then(result => {
+//         console.log('Deleted');
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+// }
 
-    return db
-      .collection("products")
-      .deleteOne({ _id: new ObjectId(prodId) })
-      .then((res) => res)
-      .catch((err) => console.log("error in deleting product", err));
-  }
-}
-
-module.exports = Product;
+// module.exports = Product;
